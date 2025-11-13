9fe8507e22d3dfc98cb6839ce21782c3872c11d1
Johannbr <johannbr@gmail.com>
2025-11-13 14:42:53 +0100

    Docker for local dev

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepotModule } from './depot/depot.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DepotModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/back/src/infra/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST', 'localhost'),
+        port: configService.get('POSTGRES_PORT', 5432),
+        username: configService.get('POSTGRES_USER', 'postgres'),
+        password: configService.get('POSTGRES_PASSWORD', 'postgres'),
+        database: configService.get('POSTGRES_DB', 'verseau2'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
