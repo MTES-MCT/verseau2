105ff8983be76e8dcb2280cf9374887f94e1f82b
Johannbr <johannbr@gmail.com>
2025-11-27 10:21:48 +0100

    feat(back): Ajout d'une VE pour les DDL auto

--- apps/back/src/infra/database/database.module.ts
+++ apps/back/src/infra/database/database.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerService } from '@shared/logger/logger.service';
+
+const getDdlSync = (configService: ConfigService) => {
+  const ddlSync = configService.get<string>('DDL_SYNC') === 'true';
+  if (ddlSync) {
+    new LoggerService('DatabaseModule').warn(`DDL_SYNC is ${ddlSync}`);
+  }
+  return ddlSync;
+};
 
 @Module({
   imports: [
@@ -14,8 +23,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         type: 'postgres',
         url: configService.getOrThrow('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: true, // TODO: à gérer selon l'environnement
-        poolSize: 30,
+        synchronize: getDdlSync(configService),
+        poolSize: 10, // TODO: à gérer selon l'environnement, web: PROCESS_TYPE=api node apps/back/dist/mainServer.js
       }),
       inject: [ConfigService],
     }),
