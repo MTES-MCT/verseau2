bc9b00805370ee5f8a7fe50fb3eef3ad5b004776
Johannbr <johannbr@gmail.com>
2025-12-15 20:08:21 +0100

    feat(migration): Migration de  depot_step_enum 'CONTROLE_COMPLETED'

---
+++ apps/back/src/infra/migrations/1765824291150-depot-step.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DepotStep1765824291150 implements MigrationInterface {
+  transaction = true;
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TYPE depot_step_enum ADD VALUE IF NOT EXISTS 'CONTROLE_COMPLETED';
+    `);
+  }
+
+  public async down(): Promise<void> {}
+}
