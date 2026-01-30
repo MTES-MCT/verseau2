eca4a5279e97b023313873005d404ee9bba742e6
Johannbr <johannbr@gmail.com>
2026-01-30 14:59:27 +0100

    fix(front): corrige la structure des titres dans IndicateursTable

--- apps/front/src/components/IndicateursTable.tsx
+++ apps/front/src/components/IndicateursTable.tsx
@@ -57,12 +57,10 @@ export function IndicateursTable() {
     <div className={fr.cx('fr-mb-4w')}>
       <div className="fr-grid-row fr-grid-row--middle fr-mb-2w">
         <div className="fr-col">
-          <h2 className="fr-h4 fr-mb-0">
-            Principaux indicateurs
-            <h3 className="fr-h6 fr-mb-0">
-              Évaluation des conformités station et système de collecte - année {indicateurs?.[0]?.annee}
-            </h3>
-          </h2>
+          <h2 className="fr-h4 fr-mb-0">Principaux indicateurs</h2>
+          <h3 className="fr-h6 fr-mb-0">
+            Évaluation des conformités station et système de collecte - année {indicateurs?.[0]?.annee}
+          </h3>
         </div>
         <div className="fr-col-auto">
           <span className="fr-badge fr-badge--info fr-badge--no-icon">Par système d'assainissement</span>
