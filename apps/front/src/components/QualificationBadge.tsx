4d6b8795dbbedcd54504fbe261ec20e8ed87acdb
Johannbr <johannbr@gmail.com>
2026-03-13 18:36:37 +0100

    feat(front): 29 - màj QualificationBadge

--- apps/front/src/components/QualificationBadge.tsx
+++ apps/front/src/components/QualificationBadge.tsx
@@ -1,10 +1,20 @@
+import type { AlertProps } from '@codegouvfr/react-dsfr/Alert';
 import { Badge } from '@codegouvfr/react-dsfr/Badge';
 
 export function QualificationBadge({ qualification }: { qualification: string | null }) {
   if (!qualification) {
     return <>-</>;
   }
-  const severity = qualification.toLowerCase().includes('qualif') ? 'success' : 'info';
+  let severity: AlertProps.Severity;
+  if (qualification.toLowerCase() === 'correcte') {
+    severity = 'success';
+  } else if (qualification.toLowerCase() === 'non qualifié') {
+    severity = 'info';
+  } else if (qualification.toLowerCase() === 'incorrecte') {
+    severity = 'error';
+  } else {
+    severity = 'info';
+  }
   return (
     <Badge severity={severity} small>
       {qualification}
